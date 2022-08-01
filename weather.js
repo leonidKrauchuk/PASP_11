5ddb375a37fb2d6e0e27843c6dcdae6521fd29a5
leonidKrauchuk <lnw@tut.by>
2022-08-01 13:01:20 +0300

    погода

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ let weather = document.querySelector(".weather");
 fetch('https://api.openweathermap.org/data/2.5/forecast?lat=52.8293&lon=26.71678&appid=38d521c818f593344c8cb1d1904bf293&units=metric&lang=ru')
 .then(function (resp){return resp.json()})
 .then(function (data){
-	console.log(data);
+	
 	// w_1.innerHTML=data.list;
 	console.log(data.list[0].main.temp);
 for (let i = 0; i < data.list.length; i ++) {
@@ -16,7 +16,7 @@ for (let i = 0; i < data.list.length; i ++) {
 	let temper = data.list[i].main.temp;
 	temper=`<span class ="spanTemper">  ${temper.toFixed(0)} &#8451 </span>`;
 	let description = data.list[i].weather[0].description;
-	console.log(description);
+	
 	let iconId = data.list[i].weather[0].icon;
 	let icon = `<img src=http://openweathermap.org/img/wn/${iconId}.png alt="icon">`;
 	let wind =`, ветер ${data.list[i].wind.speed} м/c, порывы до ${data.list[i].wind.gust} м/c`
@@ -26,7 +26,7 @@ for (let i = 0; i < data.list.length; i ++) {
 	
 };
 
-console.log(weather);
+
 })
 .catch(function(){
 //catch anny errors
